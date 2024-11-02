aa50f5b29b7bd6b710d01e654990f00083a24435
Jesús Fernández Jodar <jesus.fernandez@gesthispania.com>
2024-11-02 18:00:16 +0100

    Set page not found page content

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>Not Found</div>
+  <base-card>
+    <section>
+      <h2>Page not found</h2>
+      <p>This page could not be found, maybe <router-link to="/coaches">check out all of our coaches</router-link></p>
+    </section>
+  </base-card>
 </template>
 
 <script>
